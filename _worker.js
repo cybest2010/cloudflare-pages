74bdbce2b3b70749efab0d216009393a62f3d459
cybest2010 <971959755@qq.com>
2025-03-10 17:35:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'https://cse.google.com/cse?cx=b3fc2934b73854979'; // 将请求转发到 Google
+      url.hostname = 'https://www.baidu.com'; // 将请求转发到 Google
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
